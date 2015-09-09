b71c3d9f228e740c5e8b121cadeb8ba811594760
TalentMicro <talentmicro@TalentMicros-MacBook-Pro.local>
2015-09-09 14:49:09 +0530

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by TalentMicro on 09/09/15.
+//  Copyright (c) 2015 TalentMicro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by TalentMicro on 09/09/15.
+//  Copyright (c) 2015 TalentMicro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
