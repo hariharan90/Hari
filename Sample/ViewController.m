8ec41a7a2edb6327aa92107c4a8b5ca846feb357
TalentMicro <talentmicro@talentmicros-macbook-pro.local>
2016-01-18 11:13:22 +0530

    2

--- Sample/ViewController.m
+++ Sample/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"Test commit");
+    NSLog(@"Test commit helloo");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
